52705cd547c4cd0bfc3b95549ae0b6672ee66c17
Deepak <deepakkumardash313@gmail.com>
2025-01-06 21:03:26 +0530

    Add a quick action

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,26 @@
 import { SplashScreen, Stack } from "expo-router";
+import * as QuickActions from "expo-quick-actions";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
+
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        id: "0",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen
