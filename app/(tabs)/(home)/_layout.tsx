3e333443859e3785f3e4b76ffd95ed2bdc2a9ef1
Deepak <deepakkumardash313@gmail.com>
2025-01-06 17:55:46 +0530

    Update screen to stack

---
+++ app/(tabs)/(home)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { theme } from "@/theme";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        title: "Home",
+        headerRight: () => (
+          <Link href={"/new"} asChild>
+            <Pressable hitSlop={20}>
+              <AntDesign
+                name="pluscircleo"
+                size={24}
+                color={theme.colorGreen}
+              />
+            </Pressable>
+          </Link>
+        ),
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
